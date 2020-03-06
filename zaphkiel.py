dd55e60cb9c6965c5ccbfe5e73abf839989cd8ad
Luis Reyes Madrid <fmadrid1317@knights.ucf.edu>
2020-03-06 14:38:41 -0500

    Update WAP 1.2
    Nevermind.... strip() function was not the solution... it should be replace() .... hopefully

--- zaphkiel.py
+++ zaphkiel.py
@@ -238,8 +238,7 @@ async def currency(ctx):
 @bot.command(pass_context=True)
 async def wholesomePics(ctx):
     pics_list = ctx.message.content.split()
-    breed = str(pics_list[1:])
-    breed = breed.strip()
+    breed = str(pics_list[1:]).replce(" ","")
     url = "http://gofetch.pictures:5000/breeds/?breed="+breed
     response = requests.request("POST", url)
     data = json.loads(response.content)
