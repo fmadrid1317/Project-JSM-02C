3092508d25515b5bc7110a07de09affda065206f
Luis Reyes Madrid <fmadrid1317@knights.ucf.edu>
2020-03-06 11:15:27 -0500

    Update WAP
    Should work this time.... str() should do the trick

--- zaphkiel.py
+++ zaphkiel.py
@@ -238,8 +238,8 @@ async def currency(ctx):
 @bot.command(pass_context=True)
 async def wholesomePics(ctx):
     pics_list = ctx.message.content.split()
-    breed = pics_list[1:]
-    url = "http://gofetch.pictures:5000/breeds/?breed="+str(breed)
+    breed = str(pics_list[1:])
+    url = "http://gofetch.pictures:5000/breeds/?breed="+breed
     response = requests.request("POST", url)
     data = json.loads(response.content)
     animal_name = data[breed][0]['breed']
