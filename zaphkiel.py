fd233dee70ab361767ee717748442161927c7933
Luis Reyes Madrid <fmadrid1317@knights.ucf.edu>
2020-03-06 14:44:13 -0500

    Update zaphkiel.py
    just fixing a typo with replace()

--- zaphkiel.py
+++ zaphkiel.py
@@ -238,7 +238,7 @@ async def currency(ctx):
 @bot.command(pass_context=True)
 async def wholesomePics(ctx):
     pics_list = ctx.message.content.split()
-    breed = str(pics_list[1:]).replce(" ","")
+    breed = str(pics_list[1:]).replace(" ","")
     url = "http://gofetch.pictures:5000/breeds/?breed="+breed
     response = requests.request("POST", url)
     data = json.loads(response.content)
